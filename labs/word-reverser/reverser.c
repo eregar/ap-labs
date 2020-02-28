5541fea3ab03f2ba446a571a76a920bed762ea93
Edgar Perez <A00344473@itesm.mx>
2020-02-28 16:35:48 -0600

    solve word reverser
    Signed-off-by: Edgar Perez <A00344473@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    // Place your magic here
-    return 0;
+int main()
+{
+    int length=20;
+    int *buff=malloc(sizeof(int)*length);
+    while(1){
+        int uwu;
+        int i=0;
+        uwu=getchar();
+        while(uwu!='\n') {
+            if(uwu==-1) return 0;
+            buff[i]=uwu;
+            uwu=getchar();
+            i++;
+            if(length==i){
+                int *temp=realloc(buff,sizeof(int)*length*2);
+                if(temp){
+                    buff=temp;
+                    length*=2;
+                }
+                else{
+                    printf("memory issues, plz retry");
+                    return 0;
+                } 
+            }
+        }
+        i--;
+        while(i>=0){
+            putchar(buff[i]);
+            i--;
+        }
+        printf("\n");
+    }
+    free(buff);
 }
