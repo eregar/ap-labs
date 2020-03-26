7bcec0caa59de65751e2eb6848c0e31b79cf4a15
Edgar Perez <A00344473@itesm.mx>
2020-03-25 21:09:05 -0600

    solved string library
    Signed-off-by: Edgar Perez <A00344473@itesm.mx>

--- labs/str-lib/main.c
+++ labs/str-lib/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int mystrlen(char*);
+char *mystradd(char*, char*);
+int mystrfind(char*, char*);
+
+int main(int argc, char **argv) {
+    if(argc<3){
+	printf("se necesitan 3 argumentos");
+    }
+    else{
+        printf("Initial Length	    : %d\n",mystrlen(argv[1]));
+        char * temp=mystradd(argv[1],argv[2]);
+	printf("New String	    : %s\n",temp);
+	printf("Substring was found : %s\n",(mystrfind(temp,argv[3]))>=0? "yes":"no");
+    }
     return 0;
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,43 @@
 
 
 int mystrlen(char *str){
-    return 0;
+    int i=0;
+    while(str[i]!='\0'){
+        i++;
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int lenOrigin=mystrlen(origin);
+    int lenAddition=mystrlen(addition);
+    //char temp[lenOrigin+lenAddition];
+    int i=lenOrigin;
+    while(i<lenOrigin+lenAddition){
+        origin[i]=addition[i-lenOrigin];
+        i++;
+    }
+    return origin;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int lenOrigin=mystrlen(origin);
+    int lenSubstr=mystrlen(substr);
+    if(lenOrigin<lenSubstr || lenSubstr==0 || lenOrigin==0) return-1;
+    for(int i=0;i<lenOrigin-lenSubstr+1;i++){
+        int j=i+lenSubstr;
+        int found=1;
+        int f=0;
+        for(int k=i;k<j;k++){
+            if(origin[k]!=substr[f]){
+                found=0;
+                break;
+            }
+            f++;
+        }
+        if(found){
+            return i;
+        }
+    }
+    return -1;
 }
