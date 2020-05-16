f6e669c48cd669f84f36268e31de01eb53722ec9
Edgar Perez <A00344473@itesm.mx>
2020-05-15 19:28:42 -0500

    solved monitor
    Signed-off-by: Edgar Perez <A00344473@itesm.mx>

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,101 @@
 #include <stdio.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <stdlib.h>
 #include "logger.h"
 
+#define RED 1
+#define GREEN 2
+#define YELLOW 3
+#define WHITE 7
+#define STOUT 2
+
+int toSys=0;
+FILE *doc=NULL;
+
 int initLogger(char *logType) {
+    char log[6]="syslog";
+    int equal=1;
     printf("Initializing Logger on: %s\n", logType);
+    for(int i=0;i<5;i++){
+	if(log[i]!=logType[i]){
+	    equal=0;
+	    break;
+	}
+    }
+    if(equal){
+	if(doc==NULL){
+	    toSys=1;
+	    doc=fopen("log.txt","a");
+	    if(doc==NULL){
+		(printf("algo salio mal al abrir el archivo"));
+		toSys=0;
+	    }
+	}
+    }
     return 0;
 }
 
+void textcolor(int fg){
+    if(toSys==0){
+        char command[6];
+        sprintf(command, "\e[%dm",fg + 30);
+        printf("%s",command);
+    }
+}
+
 int infof(const char *format, ...) {
+    va_list ap;
+    va_start(ap,format);
+    textcolor(WHITE);
+    if(toSys){
+	fprintf(doc,"[INFO] ");
+	vfprintf(doc,format,ap);
+    }
+    else vprintf(format,ap);
+    va_end(ap);
     return 0;
 }
 
 int warnf(const char *format, ...) {
+    va_list ap;
+    va_start(ap,format);
+    textcolor(YELLOW);
+    if(toSys){
+	fprintf(doc,"[WARNING] ");
+	vfprintf(doc,format,ap);
+    }
+    else vprintf(format,ap);
+    va_end(ap);
+    textcolor(WHITE);
     return 0;
 }
 
 int errorf(const char *format, ...) {
+    va_list ap;
+    va_start(ap,format);
+    textcolor(RED);
+    if(toSys){
+	fprintf(doc,"[ERROR] ");
+	vfprintf(doc,format,ap);
+    }
+    else vprintf(format,ap);
+    va_end(ap);
+    textcolor(WHITE);
     return 0;
 }
 int panicf(const char *format, ...) {
+    va_list ap;
+    va_start(ap,format);
+    textcolor(GREEN);
+    if(toSys){
+	fprintf(doc,"[PANIC] ");
+	vfprintf(doc,format,ap);
+    }
+    else vprintf(format,ap);
+    va_end(ap);
+    textcolor(WHITE);
+    printf("\n");
+    raise(SIGABRT);
     return 0;
 }
-
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,71 @@
-#include <stdio.h>
-#include "logger.h"
+#define _XOPEN_SOURCE 500
+#define EVENT_SIZE  ( sizeof (struct inotify_event) )
+#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
 
-int main(){
-    // Place your magic here
+#include <ftw.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/inotify.h>
+#include "logger.h"
+int fd=-1;
+
+static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf) {
+  if(tflag == FTW_D){
+    int wd = inotify_add_watch(fd, fpath, IN_CREATE | IN_DELETE | IN_MOVE);
+    if (wd == -1) {
+      errorf("Error while adding watch to inotify");
+    }
+  }
+  return 0; 
+}
+
+int main(int argc, char **argv) {
+  if(argc!=2){
+    errorf("incorrect number of arguments");
     return 0;
+  }
+  int flag=0;
+  fd=inotify_init();
+  if(fd<0){
+    errorf("inotify cannot initiate");
+    return 0;
+  }
+  if (nftw(argv[1], display_info, 20,flag)== -1) {
+    errorf("algo salio mal");
+    exit(EXIT_FAILURE);
+  }
+
+
+  while(1){
+    char buffer[EVENT_BUF_LEN];
+    int length = read(fd, buffer, EVENT_BUF_LEN); 
+    if (length<0) {
+      errorf( "read cannot be anymore xd" );
+      break;
+    }  
+
+    int i=0;
+    while (i < length ) {
+      struct inotify_event *event = ( struct inotify_event * ) &buffer[i];  
+      if ( event->len ) {
+        if ( event->mask & IN_CREATE ) {
+          infof("%s was created\n", event->name);
+        }
+        else if ( event->mask & IN_DELETE ) {
+          infof("%s was deleted\n", event->name);
+        }
+        else if (event->cookie!=0){
+	  if(event->mask & IN_MOVED_FROM)
+          infof("%s was renamed to ",event->name);
+	  else infof("%s\n",event->name);
+        }
+        else{
+          infof("unknown action");
+        }
+      }
+      i += EVENT_SIZE + event->len;
+    }
+  }
+  //ooif
+  exit(EXIT_SUCCESS);
 }
